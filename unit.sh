6a295561324698f09645445e23a1abc414fdeab0
SRISHHA <89242466+SRISHHA@users.noreply.github.com>
2024-12-03 16:45:47 +0530

    Delete unit.sh

--- unit.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Running simple shell script..."
-echo "Current date: $(date +%Y-%m-%d)"
-echo "Current time: $(date +%T)"
-echo "Hello, this is my first shell script!"
-
-exit 0  # Ensure the script exits successfully
